356aa30ad6227133a8c64407b945756880e0be4d
Ai_Yuan <lyc4388@163.com>
2017-02-23 16:00:41 +0800

    init crawler

---
+++ img.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+# 图虫网g
+
+import urllib2
+import re
+from urllib import urlopen
+import uuid
+from time import time
+
+
+# 图片存储地址
+down_dir = '/Users/renjialiang/Desktop/huaban/'
+
+# 获取二级页面url
+def findUrl2(html):
+    re1 = r'https://tuchong.com/\d+/\d+/|http://\w+(?<!photos).tuchong.com/\d+/'
+    url2list = re.findall(re1, html)
+    url2lstfltr = list(set(url2list))
+    url2lstfltr.sort(key=url2list.index)
+    return url2lstfltr
+
+
+# 获取html文本
+def getHtml(url):
+    try:
+        html = urllib2.urlopen(url).read().decode('utf-8')  # 解码为utf-8
+        return html
+    except Exception, e:
+        print e
+        return ''
+
+
+if __name__ == '__main__':
+    query_string = raw_input('请输入要查询的关键词：')
+    pageNo = int(input('从第一页下载到多少页：'))
+    start_time = time()
+    i = 1
+    count=0
+    while i <= int(pageNo):
+        if i == 1:
+            html = getHtml("https://tuchong.com/tags/"+query_string+"/?type=new")
+        else:
+            html = getHtml("https://tuchong.com/tags/"+query_string+"/?type=new&page=" + str(i))
+
+        if html != '':
+            detllst = findUrl2(html)
+            for detail in detllst:
+                html2 = getHtml(detail)
+                re2 = r'https://photo.tuchong.com/\d+/f/\d+\.jpg'
+                imglist = re.findall(re2, html2)
+                for l in imglist:
+                    count += 1
+                    img_net = urlopen(l).read()  # 打开网络图片
+                    img_name = down_dir + str(uuid.uuid1()) + '.jpg'
+                    img_local = open(img_name, 'wb')  # 打开本地文件
+                    print '正在下载第 ' + str(count) + ' 张'
+                    img_local.write(img_net)  # 将网络文件写入到本地
+                    img_local.close()  # 关闭本地文件
+        i+=1
+
+    end_time = time()
+    print('共下载%s张素材，耗时%.2fs' % (count, end_time - start_time))
---
+++ img3.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# 花瓣网
+
+import re
+import os
+import requests
+from time import sleep, time
+from random import choice
+from multiprocessing.dummy import Pool as ThreadPool
+
+# 图片存储地址
+down_dir = '/Users/renjialiang/Desktop/huaban/'
+
+page_count = 0
+photo_number = 0
+down_data = []
+
+UserAgent = [
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',
+    'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0',
+    'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+    'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',
+    'Mozilla/5.0 (iPhone; U; CPU like Mac OS X) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/4A93 Safari/419.3',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13'
+]
+
+user_agent = choice(UserAgent)
+head = {'User-Agent': user_agent}
+TimeOut = 30
+
+
+
+def downfile(down_data):
+    print u"开始下载：", down_data[0], down_data[1]
+    try:
+        resource = requests.get(down_data[1], stream=True, headers=head).content
+        with open(down_data[0], 'wb') as file:
+            file.write(resource)
+    except Exception as e:
+        print("下载失败", e)
+
+
+def request_page_text(url):
+    try:
+        Page = requests.session().get(url, headers=head, timeout=TimeOut)
+        Page.encoding = 'utf-8'
+        return Page.text
+    except Exception as e:
+        print("请求失败了...重试中(5s)", e)
+        sleep(5)
+        print("暂停结束")
+        request_page_text(url)
+
+
+def request_url_download(url):
+    print(url)
+    global page_count
+    page_count += 1
+    global photo_number
+    text = request_page_text(url)
+    pattern = re.compile('{"pin_id":(\d*?),.*?"key":"(.*?)",.*?"like_count":(\d*?),.*?"repin_count":(\d*?),.*?}', re.S)
+    # 参数re.S 是正则表达式，编译参数标识re.DOTALL，即.匹配除、\n 所有字符
+
+    img_query_items = re.findall(pattern, text)
+
+    for url_items in img_query_items:
+        max_pin_id = url_items[0]
+        x_key = url_items[1]
+        x_like_count = int(url_items[2])
+        x_repin_count = int(url_items[3])
+        if (x_repin_count > 10 and x_like_count > 10) or x_repin_count > 100 or x_like_count > 20:
+            print("开始下载第{0}张图片".format(photo_number))
+            url_item = url_image + x_key
+            filename = down_dir + str(max_pin_id) + ".jpg"
+            if os.path.isfile(filename):
+                print("文件存在：", filename)
+                continue
+            if photo_number >= image_numbers:
+                return
+            down_data.append([filename, url_item])
+            photo_number += 1
+        else:
+            break
+    if len(img_query_items) != 0:
+        request_url_download(url_query + str(page_count))
+    return down_data
+
+
+if __name__ == '__main__':
+    start_time = time()
+    url_image = 'http://hbimg.b0.upaiyun.com/'
+    query_string = raw_input('请输入要查询的关键词：')
+    global image_numbers
+    image_numbers = int(input('下载多少张：'))
+    url_query = "http://huaban.com/search/?q=" + query_string + "&per_page=20&wfl=1&page="
+    if not os.path.exists(down_dir):
+        os.makedirs(down_dir)
+        os.chdir(down_dir)
+    else:
+        os.chdir(down_dir)
+    s = request_url_download(url_query + str(page_count))
+    pool = ThreadPool(10)
+    list(pool.map(downfile, s))
+    pool.close()
+    pool.join()
+    end_time = time()
+    print('共下载%s张素材，耗时%.2fs' % (len(s), end_time - start_time))
